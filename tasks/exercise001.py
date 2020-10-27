b1b19b63596377618bbe46fca0dde06bf2b9b9c7
yelizkilinc <55741960+yelizkilinc@users.noreply.github.com>
2020-10-27 18:10:43 +0000

    Update exercise001.py

--- tasks/exercise001.py
+++ tasks/exercise001.py
@@ -2,15 +2,14 @@
 # Your task will be to return the sum of the numbers that occur only once.
 # For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15.
 # More examples in the test cases below.
-
 # Good luck!
-
 def repeats(arr):
-    return  for item in arr:
+  dict={}
+  for item in arr:
+    if item in dict:
       dict[item]+=1
     else:
       dict[item]=0
-  
   sum=0
   for num in dict.keys():
     if dict[num]==0:
